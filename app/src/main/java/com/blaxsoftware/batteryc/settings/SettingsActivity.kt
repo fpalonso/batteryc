c47c251f1d143109fe486f5e47e4dc1b05a2d857
fpalonso <blax.software@gmail.com>
2017-07-30 08:49:45 +0200

    Initial commit. Empty project except for settings layout and activity

---
+++ app/src/main/java/com/blaxsoftware/batteryc/settings/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.blaxsoftware.batteryc.settings
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.blaxsoftware.batteryc.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
